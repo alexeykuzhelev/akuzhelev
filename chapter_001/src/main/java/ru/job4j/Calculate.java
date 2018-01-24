0d4a633355a595487af56dde00c8a70d3ae572d4
alexeykuzhelev <aleks2kv1977@gmail.com>
2018-01-24 23:21:16 +0300

    1.3. JavaDoc. Code convention [#4087]

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,6 +1,18 @@
 package ru.job4j;
 
+/**
+* Class Calculate Класс для вывода сообщения в консоль.
+*
+* @author Alexey Kuzhelev (aleks2kv1977@gmail.com)
+* @version $Id$
+* @since 24.01.2018
+*/
+
 public class Calculate {
+	/**
+    * Конструктор, вывод строки в консоль.
+    * @param args - args.
+    */
 	public static void main(String[] args) {
 		System.out.println("Hello World.");
 	}
---
+++ chapter_001/src/main/java/ru/job4j/package-info.java
@@ -0,0 +1,8 @@
+/**
+* Package for calculate task.
+*
+* @author Alexey Kuzhelev (aleks2kv1977@gmail.com)
+* @version $Id$
+* @since 24.01.2018
+*/
+package ru.job4j;
