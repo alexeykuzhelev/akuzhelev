f1859fdb28d184712ea8b5f72e84cd0906ceece8
alexeykuzhelev <aleks2kv1977@gmail.com>
2018-02-21 22:03:07 +0300

    3.3. Идеальный вес. [#41584]

--- chapter_001/src/main/java/ru/job4j/fit/Fit.java
+++ chapter_001/src/main/java/ru/job4j/fit/Fit.java
@@ -4,6 +4,9 @@ package ru.job4j.fit;
  * Программа расчета идеального веса.
  */
 public class Fit {
+    private final int HUNDRED = 100;
+    private final int HUNDRED_TEN = 110;
+    private final double COEF = 1.15;
     private double result;
 
     /**
@@ -12,7 +15,7 @@ public class Fit {
      * @return идеальный вес.
      */
     double manWeight(double height) {
-        result = (height - 100) * 1.15;
+        result = (height - HUNDRED) * COEF;
 		return this.result;
     }
 
@@ -22,7 +25,7 @@ public class Fit {
      * @return идеальный вес.
      */
     double womanWeight(double height) {
-        result = (height - 110) * 1.15;
+        result = (height - HUNDRED_TEN) * COEF;
 		return this.result;
     }
 }
