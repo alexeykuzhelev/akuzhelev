7bb2f485f39153db3bd9191546a458ece2bccdd5
alexeykuzhelev <aleks2kv1977@gmail.com>
2018-03-10 22:17:58 +0300

    4.2. Создать программу вычисляющую факториал. [#193]

---
+++ chapter_001/src/main/java/ru/job4j/loop/Factorial.java
@@ -0,0 +1,23 @@
+package ru.job4j.loop;
+
+/**
+ * @author Alexey Kuzhelev (aleks2kv1977@gmail.com)
+ * @version $Id$
+ * @since 10.03.2018
+ */
+
+/**
+ * Вычислить факториал для числа n.
+ */
+
+public class Factorial {
+	// в метод приходит положительное целое число n, сам метод должен возвращать рассчитанный 
+	// факториал для этого числа 
+	public int calc(int n) {
+		int fact = 1;
+		for (int number = 1; number <= n; ++number) { // Умножаем, пока number <= числу n.
+			fact *= number;                          // это сокращение для fact = fact * number
+		}
+		return fact;                            // возвращаем методу вычисленный выше факториал
+	}
+}
