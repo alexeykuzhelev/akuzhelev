97daccf0e7b5b3a3c3939da6f7b509ec0e8f27a2
alexeykuzhelev <aleks2kv1977@gmail.com>
2018-03-21 00:51:48 +0300

    4.5. Рефакторинг кода. [#33459]

--- chapter_001/src/main/java/ru/job4j/loop/Paint.java
+++ chapter_001/src/main/java/ru/job4j/loop/Paint.java
@@ -1,39 +1,53 @@
 package ru.job4j.loop;
 
+import java.util.function.BiPredicate;
+
 /**
  * @author Alexey Kuzhelev (aleks2kv1977@gmail.com)
  * @version $Id$
- * @since 18.03.2018
+ * @since 20.03.2018
  */
 
 public class Paint {
     /**
      * Построить пирамиду в псевдографике.
      */
+    public String rightTrl(int height) {
+        return this.loopBy(
+                height,
+                height,
+                (row, column) -> row >= column
+        );
+    }
+
+    public String leftTrl(int height) {
+        return this.loopBy(
+                height,
+                height,
+                (row, column) -> row >= height - column - 1
+        );
+    }
+
     public String pyramid(int height) {
-        // Буфер для результата.
+        return this.loopBy(
+                height,
+                2 * height - 1,
+                (row, column) -> row >= height - column - 1 && row + height - 1 >= column
+        );
+    }
+
+    private String loopBy(int height, int weight, BiPredicate<Integer, Integer> predict) {
         StringBuilder screen = new StringBuilder();
-        //вычисляем ширину пирамиды
-        int weight = 2 * height - 1;
-        //внешний цикл двигается по строкам.
         for (int row = 0; row != height; row++) {
-            //внутренний цикл определяет положение ячейки в строке.
             for (int column = 0; column != weight; column++) {
-                //Для левой стороны галки выставляются с левого края, поэтому мы должны брать высоту,
-                //равную ширине левой стороны пирамиды и вычитать текущий указатель на ячейку.
-                //Так же, мы берем - 1 потому, что элементы в массиве начинаются с 0, а не с 1.
-                //Для правой стороны мы ставим галки от центральной оси, поэтому сдвигаем на height - 1.
-                //Так мы определяем, сколько галок будет на строке.
-                if (row >= height - column - 1 && row + height - 1 >= column) {
+                if (predict.test(row, column)) {
                     screen.append("^");
                 } else {
                     screen.append(" ");
                 }
             }
-            // Добавляем перевод строки.
             screen.append(System.lineSeparator());
         }
-        // Получаем результат.
         return screen.toString();
     }
 }
