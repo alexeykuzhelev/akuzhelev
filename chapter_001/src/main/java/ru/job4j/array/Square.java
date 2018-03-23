4ed0f3b1df746786763612f7283e24679d8d7981
alexeykuzhelev <aleks2kv1977@gmail.com>
2018-03-23 21:55:19 +0300

    5.0. Заполнить массив степенями чисел. [#33488]

---
+++ chapter_001/src/main/java/ru/job4j/array/Square.java
@@ -0,0 +1,21 @@
+package ru.job4j.array;
+
+/**
+ * @author Alexey Kuzhelev (aleks2kv1977@gmail.com)
+ * @version $Id$
+ * @since 23.03.2018
+ */
+
+public class Square {
+    /**
+     * Заполнить массив степенями чисел.
+     */
+    public int[] calculate(int bound) {
+        int[] result = new int[bound];
+        // заполнить массив через цикл элементами от 1 до bound возведенными в квадрат
+        for(int i = 0; i < bound; i++) {
+            result[i] = ++i * i--;
+        }
+        return result;
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/array/SquareTest.java
@@ -0,0 +1,43 @@
+package ru.job4j.array;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * @author Alexey Kuzhelev (aleks2kv1977@gmail.com)
+ * @version $Id$
+ * @since 23.03.2018
+ */
+
+public class SquareTest {
+    @Test
+    public void whenBound3Then149() {
+        //тест, проверяющий заполняемость массива из трех элементов числами возведенными в квадрат
+        int bound = 3;
+        Square square = new Square();
+        int[] result = square.calculate(bound);
+        int[] expect = new int[] {1, 4, 9};
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenBound3Then149162536() {
+        //тест, проверяющий заполняемость массива из шести элементов числами возведенными в квадрат
+        int bound = 6;
+        Square square = new Square();
+        int[] result = square.calculate(bound);
+        int[] expect = new int[] {1, 4, 9, 16, 25, 36};
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenBound3Then14() {
+        //тест, проверяющий заполняемость массива из двух элементов числами возведенными в квадрат
+        int bound = 2;
+        Square square = new Square();
+        int[] result = square.calculate(bound);
+        int[] expect = new int[] {1, 4};
+        assertThat(result, is(expect));
+    }
+}
