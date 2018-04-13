e948cce4115154d068fc72ab62c238f851e98930
alexeykuzhelev <aleks2kv1977@gmail.com>
2018-04-13 22:02:04 +0300

    5.5. Слова начинается с ... [#41585]

---
+++ chapter_001/src/main/java/ru/job4j/array/ArrayChar.java
@@ -0,0 +1,43 @@
+package ru.job4j.array;
+
+/**
+ * @author Alexey Kuzhelev (aleks2kv1977@gmail.com)
+ * @version $Id$
+ * @since 13.04.2018
+ */
+
+/**
+ * Обертка над строкой.
+ */
+public class ArrayChar {
+    private char[] data;
+    /*Преобразуем строку line в новый массив символов и присвоим его
+    массиву data.
+    @param line - строка, содержащая полное слово*/
+    public ArrayChar(String line) {
+        this.data = line.toCharArray();
+    }
+
+    /**
+     * Метод проверяет, что слово начинается с префикса.
+     * @param prefix префикс - строка содержащая часть слова.
+     * @return - истинность или ложность того, что слово начинаеться с префикса.
+     */
+    public boolean startsWith(String prefix) {
+        boolean result = true;
+        /*Преобразуем строку prefix в новый массив символов и присвоим его
+        массиву value.*/
+        char[] value = prefix.toCharArray();
+        //Проверить, что массив data имеет первые элементы одинаковые с value.
+        //Цикл выполняется, пока не дойдем до конца префикса.
+        for(int i = 0; i < value.length; i++){
+            if(data[i] != value[i]){
+                //если находит несоответствие первых элементов, возвращает false
+                result = false;
+            }
+        }
+        /*Если все первые элементы массива data и массива value равны,
+        то возвращает true*/
+        return result;
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/array/ArrayCharTest.java
@@ -0,0 +1,29 @@
+package ru.job4j.array;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * @author Alexey Kuzhelev (aleks2kv1977@gmail.com)
+ * @version $Id$
+ * @since 13.04.2018
+ */
+
+public class ArrayCharTest {
+    @Test
+    public void whenStartWithPrefixThenTrue() {
+        //тест проверяет, что слово начинаеться с префикса
+        ArrayChar word = new ArrayChar("Hello");
+        boolean result = word.startsWith("He");
+        assertThat(result, is(true));
+    }
+
+    @Test
+    public void whenNotStartWithPrefixThenFalse() {
+        //тест проверяет, что слово не начинаеться с префикса
+        ArrayChar word = new ArrayChar("Hello");
+        boolean result = word.startsWith("Hi");
+        assertThat(result, is(false));
+    }
+}
