2a63c510a28038fced6036d3e2b03db6863e7f3d
alexeykuzhelev <aleks2kv1977@gmail.com>
2018-04-17 23:17:06 +0300

    5.6. Удаление дубликатов в массиве. [#225]

---
+++ chapter_001/src/test/java/ru/job4j/array/ArrayDuplicateTest.java
@@ -0,0 +1,23 @@
+package ru.job4j.array;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * @author Alexey Kuzhelev (aleks2kv1977@gmail.com)
+ * @version $Id$
+ * @since 17.04.2018
+ */
+
+public class ArrayDuplicateTest {
+	@Test
+	public void whenRemoveDuplicatesThenArrayWithoutDuplicate() {
+		//тест, проверяющий удаление дубликатов строк из массива строк.
+		ArrayDuplicate dup = new ArrayDuplicate();
+		String[] input = {"Привет", "Вася", "Привет", "Мир", "Вася"};
+		String[] result = dup.remove(input);
+		String[] expect = {"Привет", "Вася", "Мир"};
+		assertThat(result, is(expect));
+	}
+}
