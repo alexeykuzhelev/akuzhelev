a5ab4fe633e741fa8853671639c8f507c9dd6a05
alexeykuzhelev <aleks2kv1977@gmail.com>
2019-01-21 23:51:16 +0300

    1. Конвертация листа массивов в один лист Integer [#10037]

--- chapter_003/src/main/java/ru/job4j/list/ConvertList2Array.java
+++ chapter_003/src/main/java/ru/job4j/list/ConvertList2Array.java
@@ -1,11 +1,12 @@
 package ru.job4j.list;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * @author Alexey Kuzhelev (aleks2kv1977@gmail.com)
  * @version $Id$
- * @since 16.12.2018
+ * @since 21.01.2019
  */
 
 /**
@@ -34,5 +35,20 @@ public class ConvertList2Array {
             }
         }
         return array;
+    }
+
+    /**
+     * Метод конвертирует лист массивов в один лист Integer.
+     * @param list - список массивов.
+     * @return - общий лист Integer.
+     */
+    public List<Integer> convert(List<int[]> list) {
+        List<Integer> result = new ArrayList<>();
+        for (int[] array: list) {
+            for (int value: array) {
+                result.add(value);
+            }
+        }
+        return result;
     }
 }
--- chapter_003/src/test/java/ru/job4j/list/ConvertList2ArrayTest.java
+++ chapter_003/src/test/java/ru/job4j/list/ConvertList2ArrayTest.java
@@ -2,7 +2,9 @@ package ru.job4j.list;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
@@ -10,11 +12,11 @@ import static org.junit.Assert.assertThat;
 /**
  * @author Alexey Kuzhelev (aleks2kv1977@gmail.com)
  * @version $Id$
- * @since 16.12.2018
+ * @since 21.01.2019
  */
 
 /**
- * Класс тестирует конвертацию ArrayList в двумерный массив.
+ * Класс тестирует конвертацию ArrayList в двумерный массив и лист массивов в один лист Integer.
  */
 public class ConvertList2ArrayTest {
     /**
@@ -50,6 +52,20 @@ public class ConvertList2ArrayTest {
                 {3, 4},
                 {5, 0}
         };
+        assertThat(result, is(expect));
+    }
+
+    /**
+     * Тест конвертации листа массивов в один лист Integer (метод convert).
+     */
+    @Test
+    public void when2ArrayThen1Integer() {
+        List<int[]> list = new ArrayList<>();
+        list.add(new int[]{1, 2});
+        list.add(new int[]{3, 4, 5, 6});
+        ConvertList2Array convertList = new ConvertList2Array();
+        List<Integer> result = convertList.convert(list);
+        List<Integer> expect = Arrays.asList(1, 2, 3, 4, 5, 6);
         assertThat(result, is(expect));
     }
 }
