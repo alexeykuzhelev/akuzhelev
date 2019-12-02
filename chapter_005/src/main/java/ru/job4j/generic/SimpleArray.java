006da2da7e7b57088addb06e44b9fd3453c4b17b
alexeykuzhelev <aleks2kv1977@gmail.com>
2019-12-03 00:29:12 +0300

    5.2.1. Реализовать SimpleArray<T> [#156]

---
+++ chapter_005/src/main/java/ru/job4j/generic/SimpleArray.java
@@ -0,0 +1,105 @@
+package ru.job4j.generic;
+
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+/**
+ * @author Alexey Kuzhelev (aleks2kv1977@gmail.com)
+ * @version $Id$
+ * @since 03.12.2019
+ */
+
+/**
+ * Класс реализует универсальную обертку над массивом.
+ */
+public class SimpleArray<T> implements Iterable<T> {
+
+    private final Object[] objects;
+    private int index = 0;
+
+    public SimpleArray(int size) {
+        this.objects = new Object[size];
+    }
+
+    /**
+     * Метод добавляет указанный элемент (model) в первую свободную ячейку.
+     */
+    public void add(T model) throws ArrayIndexOutOfBoundsException {
+        this.objects[index++] = model;
+    }
+
+    /**
+     * Метод заменяет указанным элементом (model) элемент, находящийся по индексу index.
+     */
+    public void set(int position, T model) throws ArrayIndexOutOfBoundsException {
+        checkSizeContainer(position);
+        this.objects[position] = model;
+    }
+
+    /**
+     * Метод удаляет элемент по указанному индексу.
+     * Все находящиеся справа элементы при этом необходимо сдвинуть на единицу влево.
+     */
+    public void remove(int position) throws ArrayIndexOutOfBoundsException {
+        checkSizeContainer(position);
+        System.arraycopy(this.objects, position + 1, this.objects,
+                position, index - position);
+        this.objects[index] = null;
+        index--;
+    }
+
+    /**
+     * Метод возвращает элемент, расположенный по указанному индексу.
+     */
+    public T get(int position) {
+        return (T) this.objects[position];
+    }
+
+    /**
+     * Метод возвращает итератор, предназначенный для обхода данной структуры.
+     */
+    @Override
+    public Iterator<T> iterator() {
+        return new SimpleArrayIterator(index,
+                (Iterator<T>) Arrays.asList(this.objects).iterator());
+    }
+
+    /**
+     * Метод выкидывает исключение, если индекс выходит за границу массива.
+     */
+    private void checkSizeContainer(int position) throws ArrayIndexOutOfBoundsException {
+        if (position > index || index == 0) {
+            throw new ArrayIndexOutOfBoundsException("container empty or full");
+        }
+    }
+
+    /**
+     * Класс реализует итератор для массива.
+     */
+    private class SimpleArrayIterator implements Iterator<T> {
+
+        private final int size;
+        private int index = 0;
+        private final Iterator<T> arrayIterator;
+
+        public SimpleArrayIterator(int size, Iterator<T> arrayIterator) {
+            this.size = size;
+            this.arrayIterator = arrayIterator;
+        }
+
+        @Override
+        public boolean hasNext() {
+            return (index <= size - 1) && arrayIterator.hasNext();
+        }
+
+        @Override
+        public T next() {
+            if (!hasNext()) {
+                throw new NoSuchElementException("no any next element");
+            }
+            index++;
+            return arrayIterator.next();
+        }
+    }
+}
---
+++ chapter_005/src/test/java/ru/job4j/generic/SimpleArrayTest.java
@@ -0,0 +1,97 @@
+package ru.job4j.generic;
+
+import org.junit.Test;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
+
+/**
+ * @author Alexey Kuzhelev (aleks2kv1977@gmail.com)
+ * @version $Id$
+ * @since 03.12.2019
+ */
+
+/**
+ * Класс тестирует универсальную обертку над массивом.
+ */
+public class SimpleArrayTest {
+
+    @Test
+    public void whenAddElementsInContainerShouldGetSameElements() {
+        SimpleArray<String> simpleArray = new SimpleArray<>(1);
+        simpleArray.add("test");
+        String result = simpleArray.get(0);
+        assertThat(result, is("test"));
+    }
+
+    @Test(expected = ArrayIndexOutOfBoundsException.class)
+    public void whenAddElementInFullContainerShouldReturnException() {
+        SimpleArray<String> simpleArray = new SimpleArray<>(1);
+        simpleArray.add("test1");
+        simpleArray.add("test2");
+    }
+
+    @Test
+    public void whenAddAndSetElementInContainerShouldGetSetElement() {
+        SimpleArray<String> simpleArray = new SimpleArray<>(1);
+        simpleArray.add("test1");
+        simpleArray.set(0, "test2");
+        String result = simpleArray.get(0);
+        assertThat(result, is("test2"));
+    }
+
+    @Test(expected = ArrayIndexOutOfBoundsException.class)
+    public void whenSetWithoutAddElementsShouldReturnException() {
+        SimpleArray<String> simpleArray = new SimpleArray<>(2);
+        simpleArray.set(0, "test");
+    }
+
+    @Test(expected = ArrayIndexOutOfBoundsException.class)
+    public void whenRemoveWithoutAddElementsShouldReturnException() {
+        SimpleArray<String> simpleArray = new SimpleArray<>(2);
+        simpleArray.remove(0);
+    }
+
+    @Test
+    public void whenAddAndRemove1ElementInContainerShouldGetNull() {
+        SimpleArray<String> simpleArray = new SimpleArray<>(2);
+        simpleArray.add("test");
+        simpleArray.remove(0);
+        assertNull(simpleArray.get(0));
+    }
+
+    @Test
+    public void whenAdd3AndRemoveMiddleElementInContainerShouldGetLastElement() {
+        SimpleArray<String> simpleArray = new SimpleArray<>(4);
+        simpleArray.add("test1");
+        simpleArray.add("test2");
+        simpleArray.add("test3");
+        simpleArray.remove(1);
+        assertThat(simpleArray.get(1), is("test3"));
+    }
+
+    @Test
+    public void whenAdd2ElementsInContainerShouldGet2ElementsUsingIterator() {
+        SimpleArray<String> simpleArray = new SimpleArray<>(2);
+        simpleArray.add("test1");
+        simpleArray.add("test2");
+        String[] expected = new String[]{"test1", "test2"};
+        String[] result = new String[2];
+        Iterator<String> iterator = simpleArray.iterator();
+        result[0] = iterator.next();
+        result[1] = iterator.next();
+        assertThat(result, is(expected));
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void whenUsingIteratorNoNextElementShouldReturnException() {
+        SimpleArray<String> simpleArray = new SimpleArray<>(1);
+        simpleArray.add("test1");
+        Iterator<String> iterator = simpleArray.iterator();
+        iterator.next();
+        iterator.next();
+    }
+}
