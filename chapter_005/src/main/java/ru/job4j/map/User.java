8c774f40692499ed093bc1513e678b2651971f04
alexeykuzhelev <aleks2kv1977@gmail.com>
2020-03-01 23:42:56 +0300

    1. Создать модель User [#999]

---
+++ chapter_005/src/main/java/ru/job4j/map/User.java
@@ -0,0 +1,25 @@
+package ru.job4j.map;
+
+import java.util.Calendar;
+
+/**
+ * @author Alexey Kuzhelev (aleks2kv1977@gmail.com)
+ * @version $Id$
+ * @since 01.03.2020
+ */
+
+/**
+ * Класс реализует модель User.
+ */
+public class User {
+
+    private String name;
+    private int children;
+    private Calendar birthday;
+
+    public User(String name, int children, Calendar birthday) {
+        this.name = name;
+        this.children = children;
+        this.birthday = birthday;
+    }
+}
