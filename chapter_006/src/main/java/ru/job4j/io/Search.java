7e0e467a1b75d98b0743b4fb6217589af508e011
alexeykuzhelev <aleks2kv1977@gmail.com>
2021-04-17 23:21:59 +0300

    5. Валидация параметров запуска. [#246865]

--- chapter_006/src/main/java/ru/job4j/io/Search.java
+++ chapter_006/src/main/java/ru/job4j/io/Search.java
@@ -10,18 +10,27 @@ import java.util.function.Predicate;
 /*
   @author Alexey Kuzhelev (aleks2kv1977@gmail.com)
  * @version $Id$
- * @since 14.03.2021
+ * @since 17.04.2021
  */
 
 /**
  * Класс реализует программу поиска файлов по определенному предикату.
  * Программа возвращает файлы с расширением js в директории и поддиректориях.
  * Для поиска и обработки файлов использован класс SearchFiles.
+ * Программа доработана для запуска с параметрами.
+ * Первый параметр - начальная папка.
+ * Второй параметр - расширение файлов, которые нужно искать.
+ * Реализована также валидация параметров запуска.
  */
 public class Search {
     public static void main(String[] args) throws IOException {
-        Path start = Paths.get(".");
-        search(start, p -> p.toFile().getName().endsWith("js")).forEach(System.out::println);
+        if (args.length == 0) {
+            throw new IllegalArgumentException(
+                    "Root folder is null. Usage java -jar dir.jar ROOT_FOLDER."
+            );
+        }
+        Path start = Paths.get(args[0]);
+        search(start, p -> p.toFile().getName().endsWith(args[1])).forEach(System.out::println);
     }
 
     public static List<Path> search(Path root, Predicate<Path> condition) throws IOException {
