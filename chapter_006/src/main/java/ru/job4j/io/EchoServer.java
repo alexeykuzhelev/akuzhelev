2c12c9684ef3ab776e19ed4806db8e6f9456148c
alexeykuzhelev <aleks2kv1977@gmail.com>
2021-07-25 19:49:23 +0300

    1. Бот [#7921]

--- chapter_006/src/main/java/ru/job4j/io/EchoServer.java
+++ chapter_006/src/main/java/ru/job4j/io/EchoServer.java
@@ -7,14 +7,16 @@ import java.net.Socket;
 /*
   @author Alexey Kuzhelev (aleks2kv1977@gmail.com)
  * @version $Id$
- * @since 11.07.2021
+ * @since 25.07.2021
  */
 
 /**
  * Класс реализует обмен сообщениями через класс Socket.
- * В качестве клиента используется программа cURL.
+ * В качестве клиента используется Браузер.
  * Класс ServerSocket создает сервер.
- * Если клиент отправляет запрос с параметром msg=Bye, нужно завершить работу сервера.
+ * Если клиент отправляет запрос с параметром msg=Exit, нужно завершить работу сервера.
+ * Если клиент отправляет запрос с параметром msg=Hello, ответ будет: Hello, dear friend.
+ * Если клиент отправляет запрос с любым другим параметром msg, в ответ отправлять текст запроса.
  */
 public class EchoServer {
     public static void main(String[] args) throws IOException {
@@ -28,11 +30,13 @@ public class EchoServer {
                     out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
                     for (String str = in.readLine(); str != null && !str.isEmpty(); str = in.readLine()) {
                         String[] strings = str.replaceAll("=", " ").split(" ");
-                        if (str.contains("msg") && str.contains("Bye")) {
+                        if (str.contains("msg") && str.contains("Exit")) {
                             socket.close();
                             System.out.println("Сокет закрыт");
                             System.out.println();
-                        } else if (str.contains("msg") && !str.contains("Bye")) {
+                        } else if (str.contains("msg") && str.contains("Hello")) {
+                            out.write("Hello, dear friend.".getBytes());
+                        } else if (str.contains("msg") && !str.contains("Exit") && !str.contains("Hello")) {
                             out.write((strings[2] + "\r\n\r\n").getBytes());
                         }
                         System.out.println(str);
