90d948b037bb3fa4ab9a1b5f6b42e0884d983056
alexeykuzhelev <aleks2kv1977@gmail.com>
2022-08-03 02:14:31 +0300

    0. JDBC [#6863]

---
+++ chapter_007/src/main/java/ru/job4j/jdbc/ConnectionDemo.java
@@ -0,0 +1,34 @@
+package ru.job4j.jdbc;
+
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import ru.job4j.io.Config;
+
+/*
+  @author Alexey Kuzhelev (aleks2kv1977@gmail.com)
+ * @version $Id$
+ * @since 02.08.2022
+ */
+
+/**
+ * Класс реализует чтение файла конфигурации, чтобы получить данные для подключения к БД.
+ * Устанавливает подключение к БД и получает данные о подключении, такие как имя пользователя и url.
+ */
+public class ConnectionDemo {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        Config config = new Config("./chapter_007/src/main/resources/app.properties");
+        config.load();
+        Class.forName(config.value("driver"));
+        String url = config.value("url");
+        String login = config.value("login");
+        String password = config.value("password");
+        try (Connection connection = DriverManager.getConnection(url, login, password)) {
+            DatabaseMetaData metaData = connection.getMetaData();
+            System.out.println(metaData.getUserName());
+            System.out.println(metaData.getURL());
+        }
+    }
+}
