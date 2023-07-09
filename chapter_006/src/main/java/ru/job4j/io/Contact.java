c0830e8bae4bda2c055685f8bf26e3ad0603ab8e
alexeykuzhelev <aleks2kv1977@gmail.com>
2023-07-10 00:18:12 +0300

    2. Триггеры [#504803] Исправление ошибок CI Bot

--- chapter_006/src/main/java/ru/job4j/io/Contact.java
+++ chapter_006/src/main/java/ru/job4j/io/Contact.java
@@ -43,10 +43,10 @@ public class Contact {
             + '}';
     }
 
-/*    public static void main(String[] args) throws IOException, ClassNotFoundException {
+    /*public static void main(String[] args) throws IOException, ClassNotFoundException {
         final Contact contact = new Contact(123456, "+7 (111) 111-11-11");
 
-        *//* Запись объекта во временный файл, который удалится системой *//*
+         Запись объекта во временный файл, который удалится системой
         File tempFile = Files.createTempFile(null, null).toFile();
         try (FileOutputStream fos = new FileOutputStream(tempFile);
              ObjectOutputStream oos =
@@ -54,7 +54,7 @@ public class Contact {
             oos.writeObject(contact);
         }
 
-        *//* Чтение объекта из файла *//*
+         Чтение объекта из файла
         try (FileInputStream fis = new FileInputStream(tempFile);
              ObjectInputStream ois =
                  new ObjectInputStream(fis)) {
